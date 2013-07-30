f022022296ca86f7f4448dd352069788f3fe66fd
ganesan <ganesan@ganesan-Vostro-220s-Series>
2013-07-30 19:22:38 +0530

    live work

--- Important/src/com/soap/SOAPClientSAAJ.java
+++ Important/src/com/soap/SOAPClientSAAJ.java
@@ -15,13 +15,14 @@ public class SOAPClientSAAJ {
             SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
             SOAPConnection soapConnection = soapConnectionFactory.createConnection();
 
-
+            String urls = "https://secure.easymedmobile.com/notifications/lab/hugs";
+            
             //no ?wsdl
             String url = "http://192.168.1.19:8080/EasyMedAPI/ws/hugs";            
-            //SOAPMessage soapget= soapConnection.get("http://192.168.1.19:8080/EasyMedAPI/ws/hugs?wsdl");
-            //printSOAPResponse(soapget);
+            SOAPMessage soapget= soapConnection.get(urls+"?wsdl");
+            printSOAPResponse(soapget);
             
-            SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(), url);            
+            SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(), urls);            
             // Process the SOAP Response
             printSOAPResponse(soapResponse);
 
@@ -54,6 +55,12 @@ public class SOAPClientSAAJ {
         </soapenv:Body>
      </soapenv:Envelope>
 */
+        
+        String hl7message="MSH|^~\\&|unilab2|32^MATPED-LB|EasyMed||20130619162139.139+0200||ORU^R01|ul2.32.3.133764.1371651699139|F|2.5||1371651699139|||CHE|8859/15|fr\n" +
+        		"PID|1||6||XXXX||||||||0041765093523|||||||||||||||||\n" +
+        		"OBR|1|4^^20130502^3001|46593|EXAM||20130502000000+0200|20130502100000+0200||0^0|||||201305021114+0200|1556^sgv^sang veineux^4||00447952414020^WPN^PH~0041783720000^BPN^CP~cardiologie@hcuge.ch^NET^Internet|N|N|||20130502111420+0200|||F||1^R\n" +
+        		"OBX|2|TX|329647^INR^29570^63496|0|NON INTERPRÉT.||||||F|||20130619162124+0200|2164^INR^20^11707^Hémostase^^Hémostase|MKF^MKF";
+        
         // SOAP Body  
         SOAPBody soapBody = envelope.getBody();
         SOAPElement soapBodyElem = soapBody.addChildElement("labResult","lab");
